9cef2b0ae7a7bb6cad2c5d2e3ca863f44e9bd153
mukul0000 <mukulrajput9675@gmail.com>
2021-11-08 14:16:56 +0530

    start working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Manag } from "./component/managment";
 function App() {
 	return (
 		<div className="App">
 			<div>hey its working</div>
+			<Manag></Manag>
 		</div>
 	);
 }
---
+++ src/component/managment.jsx
@@ -0,0 +1,8 @@
+
+
+function Manag(){
+
+    return <h1>working</h1>
+}
+
+export {Manag}
